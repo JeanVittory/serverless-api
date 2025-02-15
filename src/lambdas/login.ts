92ea1961eecd28eca139c5359aa0c3db272d20ee
JeanVittory <vittory.dev@gmail.com>
2025-02-14 20:59:02 -0500

    feat: :art: Add format using prettier configuration

--- src/lambdas/login.ts
+++ src/lambdas/login.ts
@@ -1,36 +1,38 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
-import { CognitoIdentityProvider, AuthFlowType } from "@aws-sdk/client-cognito-identity-provider";
+import {
+  CognitoIdentityProvider,
+  AuthFlowType,
+} from "@aws-sdk/client-cognito-identity-provider";
 
 const cognito = new CognitoIdentityProvider();
 
-export const handler:APIGatewayProxyHandler = async (event) =>{
-    const body  = JSON.parse(event.body || "{}")
-    const { email, password } = body
-    const params = {
-        AuthFlow: AuthFlowType.USER_PASSWORD_AUTH,
-        ClientId: process.env.COGNITO_USER_POOL_CLIENT_ID || "",
-        AuthParameters: {
-            USERNAME: email,
-            PASSWORD: password
-        }
+export const handler: APIGatewayProxyHandler = async (event) => {
+  const body = JSON.parse(event.body || "{}");
+  const { email, password } = body;
+  const params = {
+    AuthFlow: AuthFlowType.USER_PASSWORD_AUTH,
+    ClientId: process.env.COGNITO_USER_POOL_CLIENT_ID || "",
+    AuthParameters: {
+      USERNAME: email,
+      PASSWORD: password,
+    },
+  };
+  try {
+    const response = await cognito.initiateAuth(params);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response.AuthenticationResult),
+    };
+  } catch (error) {
+    if (error.name === "UserNotConfirmedException") {
+      return {
+        statusCode: error.$metadata.httpStatusCode,
+        body: JSON.stringify({ message: error.name }),
+      };
     }
-    try {
-        const response = await cognito.initiateAuth(params)
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify(response.AuthenticationResult)
-        }
-    } catch (error) {
-        if(error.name === "UserNotConfirmedException"){
-            return {
-                statusCode: error.$metadata.httpStatusCode,
-                body: JSON.stringify({message: error.name})
-            }
-        }
-        return {
-            statusCode: 500,
-            body: JSON.stringify({message:error})
-        }
-    }
-}
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: error }),
+    };
+  }
+};
