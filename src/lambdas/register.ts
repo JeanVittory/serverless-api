89110e38d763251173c44730ec43c0f2d5e411e1
JeanVittory <vittory.dev@gmail.com>
2025-02-14 20:58:31 -0500

    feat: :art: Add format using prettier configuration

--- src/lambdas/register.ts
+++ src/lambdas/register.ts
@@ -1,55 +1,53 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+import { CognitoIdentityProvider } from "@aws-sdk/client-cognito-identity-provider";
 
-import {APIGatewayProxyHandler} from "aws-lambda"
-import { CognitoIdentityProvider } from "@aws-sdk/client-cognito-identity-provider"
+const cognito = new CognitoIdentityProvider();
 
-const cognito = new CognitoIdentityProvider()
+export const handler: APIGatewayProxyHandler = async (event) => {
+  if (!event.body) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: "Missing body in request" }),
+    };
+  }
 
-export const handler:APIGatewayProxyHandler = async (event) => {
+  const body = JSON.parse(event.body || "{}");
 
-    if(!event.body){
-        return {
-            statusCode: 400,
-            body: JSON.stringify({message: "Missing body in request"})
-        }
-    }
+  const { email, password, nickname } = body;
 
-    const body = JSON.parse(event.body || "{}")
+  if (!email || !password || !nickname) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: "Missing required fields in request" }),
+    };
+  }
 
-    const {email, password, nickname} = body
+  const params = {
+    ClientId: process.env.COGNITO_USER_POOL_CLIENT_ID || "",
+    Username: email,
+    Password: password,
+    UserAttributes: [
+      {
+        Name: "email",
+        Value: email,
+      },
+      {
+        Name: "nickname",
+        Value: nickname,
+      },
+    ],
+  };
+  try {
+    await cognito.signUp(params);
 
-    if(!email || !password || !nickname){
-        return {
-            statusCode: 400,
-            body: JSON.stringify({message: "Missing required fields in request"})
-        }
-    }
-
-    const params = {
-        ClientId: process.env.COGNITO_USER_POOL_CLIENT_ID || "",
-        Username: email,
-        Password: password,
-        UserAttributes: [
-            {
-                Name: "email",
-                Value: email
-            }, 
-            {
-                Name: "nickname",
-                Value: nickname
-            }
-        ]
-    }
-    try {
-         await cognito.signUp(params)
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify({message: "User registered successfully"})
-        }
-    } catch (error) {
-       return {
-        statusCode: 500 ,
-        body: JSON.stringify({message: error.message})
-       } 
-    }
-}
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "User registered successfully" }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: error.message }),
+    };
+  }
+};
